9f3be63ef7672c13e612440edfd30e97208ba8b5
Amirali <a.sahraei98@gmail.com>
2024-07-18 16:38:55 +0400

    Fix image path bug

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 
-
 app = FastAPI()
-
 
 templates = Jinja2Templates(directory="./templates")
 app.mount("/static", StaticFiles(directory="./static"), name="static")
@@ -29,9 +27,10 @@ def read_root():
 
 # exempt from api docs so set include_in_schema to False
 @app.post("/prediction", include_in_schema=False)
-def pred_func(request: Request, image: Annotated[bytes, File()]):
+def pred_func(request: Request, image: Annotated[UploadFile, File()]):
     try:
-        content = Image.open(image).convert("RGB")
+        content = image.file.read()
+        content = Image.open(io.BytesIO(content)).convert("RGB")
         content = transformer(content)
         prediction = model_pipeline(content)
         data = {"prediction": prediction}
@@ -40,14 +39,14 @@ def pred_func(request: Request, image: Annotated[bytes, File()]):
         )
 
     except Exception as e:
-        error_msg = f"Error processing image: {str(e)}"
+        error_msg = f"Error: {str(e)}"
         return templates.TemplateResponse(
             "error.html", {"request": request, "error_msg": error_msg}
         )
 
 
 @app.post("/prediction_fastapi_doc")
-def pred_func(request: Request, image: UploadFile):
+def pred_func(request: Request, image: Annotated[UploadFile, File()]):
     try:
         content = image.file.read()
         content = Image.open(io.BytesIO(content)).convert("RGB")
@@ -57,7 +56,7 @@ def pred_func(request: Request, image: UploadFile):
         return data
 
     except Exception as e:
-        error_msg = f"Error processing image: {str(e)}"
+        error_msg = f"Error: {str(e)}"
         return templates.TemplateResponse(
             "error.html", {"request": request, "error_msg": error_msg}
         )
